6cb1e95a6abe53d097970c7d5779fb5e73b88c53
Dev Watts <59016469+devwatts@users.noreply.github.com>
2024-08-28 01:29:13 +0530

    api update

--- actions/request.js
+++ actions/request.js
@@ -1,5 +1,5 @@
 import fetch from "isomorphic-fetch";
-const API = 'https://codecave.devwatts.xyz'
+const API = 'https://codecaveapi.devwatts.xyz'
 //const API = 'http://localhost:5000';
 
 export const getLink = (dataFromTextArea,loadingFunction) => {
